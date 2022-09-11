e3dcec5675603c85393a632afe0fe6e32ece12e0
Jaejune <quswowns345@naver.com>
2022-09-11 16:16:43 +0900

    222

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client'
+import { useRouter } from 'next/router'
+
+const cache = new InMemoryCache
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  console.log(process.env.NEXT_PUBLIC_DEV_URL, process.env.NEXT_PUBLIC_PRO_URL)
+  console.log(process.env.ENV_DEV_URL, process.env.ENV_PRO_URL)
+  const client = new ApolloClient({
+    uri: `${process.env.NEXT_PUBLIC_DEV_URL}`,
+    cache
+  })
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,27 @@
+import { gql, useQuery } from "@apollo/client"
+
+const FETCH_BOARDS = gql`
+  query fetchBoards {
+      fetchBoards {
+        writer
+        title
+      }
+    }
+  
+`
+
 const FirstPage = () => {
+
+  const {data} = useQuery(FETCH_BOARDS)
+
 
   return(
     <div>
+
       첫 페이지 입니다.
+      {data?.fetchBoards.map((el, i) => (
+        <div key={i}>{el.writer}, {el.title}</div>
+      ))}
     </div>
   )
 }
